b7ad060a5f2eed02341c8fd65501599fb85bbed9
Ezenwa Onukwue <ezenwa.onukwue@salesloft.com>
2018-11-26 03:29:46 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,11 +1,11 @@
 class Backer
 attr_accessor :name, :project
-@@backed_projects = []
+@backed_projects = []
   def initialize(name)
     @name = name
   end
 
   def back_project(project)
-    @@backed_projects << self
+    @backed_projects << self
   end
 end
